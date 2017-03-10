b5963346b7e9536ca7fbf29061e7591fb47c7de5
ejunjsh <sjj050121014@163.com>
2017-03-11 01:52:41 +0800

    hh

--- hadoop-cluster-docker/start-container.sh
+++ hadoop-cluster-docker/start-container.sh
@@ -15,7 +15,7 @@ sudo docker run -itd \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                ejunjsh/hadoop:1.0 &> /dev/null
 
 
 # start hadoop slave container
@@ -29,7 +29,7 @@ do
 	                --net=hadoop \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
+	                ejunjsh/hadoop:1.0 &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
