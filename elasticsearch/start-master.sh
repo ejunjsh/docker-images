c120e712e1cd84a6a02f86def2aafd5d9f6bee31
zhouff <sjj050121014@163.com>
2017-06-20 22:20:56 +0800

    refactor elasticsearch

--- elasticsearch/start-master.sh
+++ elasticsearch/start-master.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # start elasticsearch container
 sudo sysctl -w vm.max_map_count=262144
-N=`cat nodes`
+N=3
 N=$(( $N + 1 ))
 i=1
 while [ $i -lt $N ]
 do
-        sudo docker rm -f ec$i &> /dev/null
+        sudo docker rm -f ec-master$i &> /dev/null
         sleep 1
-        echo "start ec$i container..."
+        echo "start ec-master$i container..."
         sudo docker run -itd \
                 --net=hadoop \
-                --name ec$i \
-                --hostname ec$i \
+                --name ec-master$i \
+                --hostname ec-master$i \
                 -p 920$(( $i - 1 )):9200   \
                 jackshao/elasticsearch:1.0 &> /dev/null
         sleep 1 
         i=$(( $i + 1 ))
 done
 sleep 1
-sudo docker exec -it ec1 bash
+sudo docker exec -it ec-master1 bash
---
+++ elasticsearch/start-slave.sh
@@ -0,0 +1,11 @@
+sudo docker rm -f ec-slave$1 &> /dev/null
+        sleep 1
+        echo "start ec-slave$1 container..."
+        sudo docker run -itd \
+                --net=hadoop \
+                --name ec-slave$1 \
+                --hostname ec-slave$1 \
+                jackshao/elasticsearch:1.0 &> /dev/null
+        sleep 1 
+
+sudo docker exec -it ec-slave$1 bash
